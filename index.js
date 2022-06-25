1b350e0f695d9aac40bf4139b38181d864a21a58
Matthew Mickler <matthewmickler@Matthews-MBP.hitronhub.home>
2022-06-25 15:44:13 -0700

    stoplight commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let stopLight = document.getElementById("stopLight");
+stopButton.addEventListener("click", () => {
+  stopLight.classList.toggle("stop");
+});
 
-  // YOUR CODE HERE
-})();
+let slowButton = document.getElementById("slowButton");
+let slowLight = document.getElementById("slowLight");
+slowButton.addEventListener("click", () => {
+  slowLight.classList.toggle("slow");
+});
+
+let goButton = document.getElementById("goButton");
+let goLight = document.getElementById("goLight");
+goButton.addEventListener("click", () => {
+  goLight.classList.toggle("go");
+});
+let mouseTarget = document.getElementById("mouseTarget");
+
+[stopButton, slowButton, goButton].forEach((button) => {
+  button.addEventListener("mouseenter", () => {
+    console.log(`Entered ${button.innerText} button`);
+  });
+
+  button.addEventListener("mouseleave", () => {
+    console.log(`Left ${button.innerText} button`);
+  });
+});
